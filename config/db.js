c4f681032ad49cab8bbca27802128f0bd7ce8ce3
james-zedd <james.zedd@gmail.com>
2020-04-28 17:57:43 -0400

    Updates 20200428

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB =  async () => {
+const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
@@ -10,12 +10,11 @@ const connectDB =  async () => {
       useFindAndModify: false,
       useUnifiedTopology: true
     });
-    console.log('Mongo DB connected')
+    console.log('Mongo DB connected');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
   }
-
 };
 
 module.exports = connectDB;
